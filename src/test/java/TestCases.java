29edb08a1df0ae28e178011c8104546ab89f27f2
JeniJanu <jeni.jayesh@gmail.com>
2023-12-28 13:28:38 +0530

    Initial commit

---
+++ src/test/java/TestCases.java
@@ -0,0 +1,20 @@
+import org.testng.annotations.Test;
+
+public class TestCases {
+
+    @Test
+    void test1(){
+        System.out.println("Inside test1");
+    }
+
+    @Test
+    void test2(){
+        System.out.println("Inside test2");
+    }
+
+    @Test
+    void test3(){
+        System.out.println("Inside test3");
+    }
+
+}
